79db9f15e1e50a0ff3bad86f60b8808114054b17
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2022-05-11 12:23:07 +0200

    caseopening

--- SOSU2022_BackEnd/SOSU2022_BackEnd.Core/Models/CaseOpening.cs
+++ SOSU2022_BackEnd/SOSU2022_BackEnd.Core/Models/CaseOpening.cs
@@ -1,7 +1,15 @@
+using MongoDB.Bson;
+
 namespace SOSU2022_BacEnd.Domain.IRepositories
 {
     public class CaseOpening
     {
-        
+        public ObjectId Id { get; set; }
+        public ObjectId BorgerId { get; set; }
+        public string Henvisning { get; set; }
+        public string Fritekst { get; set; }
+        public string Tilstand { get; set; }
+        public string Indsats { get; set; }
+        public string Diagnose { get; set; }
     }
 }
--- SOSU2022_BackEnd/SOSU2022_BackEnd.DataAccess/Repositories/CaseOpeningRepository.cs
+++ SOSU2022_BackEnd/SOSU2022_BackEnd.DataAccess/Repositories/CaseOpeningRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using SOSU2022_BacEnd.Domain.IRepositories;
 using SOSU2022_BackEnd.Core.Models;
@@ -14,9 +15,16 @@ namespace SOSU2022_BackEnd.DataAcces.Repositories
             var client =
                 new MongoClient(
                     "mongodb+srv://Eksamen2022:UUTu4zvESzEUcL9k@cluster0.oowmj.mongodb.net/SOSU2022?retryWrites=true&w=majority");
-            var db = client.GetDatabase("CaseOpening");
+            var db = client.GetDatabase("SOSU2022");
+            _caseOpenings = db.GetCollection<CaseOpening>("Sagsåbning");
         }
-        
+
+        public CaseOpening Create(CaseOpening caseOpening)
+        {
+            _caseOpenings.InsertOne(caseOpening);
+            return caseOpening;
+        }
+
         public List<CaseOpening> GetAll()
         {
             return _caseOpenings.Find(caseOpening => true).ToList();
