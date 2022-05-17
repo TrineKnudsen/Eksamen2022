cd864849f3180f92ba550af49fb58d7bb68b8363
TrineKnudsen <trinesaknudsen@hotmail.com>
2022-05-17 13:14:24 +0200

    Generelle oplysninger med tooltip

--- sosu-frontend/src/views/StudentView.vue
+++ sosu-frontend/src/views/StudentView.vue
@@ -36,19 +36,130 @@
   <div class="col">
     <div class="collapse multi-collapse" id="multicollapse">
       <div class="card card-body">
-        <b-list-group>
-          <b-list-group-item>Mestring</b-list-group-item>
-          <b-list-group-item>Motivation</b-list-group-item>
-          <b-list-group-item>Ressourcer</b-list-group-item>
-          <b-list-group-item>Roller</b-list-group-item>
-          <b-list-group-item>Vaner</b-list-group-item>
-          <b-list-group-item>Uddanelse og job</b-list-group-item>
-          <b-list-group-item>Livshistorie</b-list-group-item>
-          <b-list-group-item>Netværk</b-list-group-item>
-          <b-list-group-item>Helbredsoplysninger</b-list-group-item>
-          <b-list-group-item>Hjælpemidler</b-list-group-item>
-          <b-list-group-item>Boligens indretning</b-list-group-item>
-        </b-list-group>
+        <div>
+          <a>Mestring</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres, hvordan borgeren positivt eller negativt mestrer den modgang vedkommende møder."
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Motivation</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres borgerens ønsker for sit liv (overordnet mål), og hvad der motiverer borgeren."
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Ressourcer</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres de ressourcer, borgeren har i forhold til at løse dagligdagens opgaver. Det kan være både fysiske og mentale funktioner. "
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Roller</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres de roller, borgeren angiver at have. Det kan fx være rolle som ægtefælle, bedsteforælder eller aktiv i beboerforening"
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Vaner</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres borgerens vaner, som er en naturlig del af hverdagen, og som borgeren plejer at gøre. Det kan både være vaner, som borgeren gør af fysiske og psykiske årsager."
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Uddannelse og job</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres borgerens oplysninger om uddannelse og erhverv. Fx hvilke job borgeren har haft, hvilken betydning arbejdslivet har haft for borgeren, og har borgeren evt. stadig tilknytning til arbejdspladsen og kolleger"
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Livshistorie</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres borgerens fortælling om sit liv.
+            Her dokumenteres borgerens ønsker for den sidste tid. "
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Netværk</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres borgerens netværk i bredere forstand."
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Helbredsoplysninger</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her kan borgerens sygdomme og handicap dokumenteres for at give et samlet overblik.
+            Hvis oplysningen kommer fra borgeren eller pårørende, skal dette fremgå.
+            Her kan behandlingsansvarlig læge/ambulatorie ift. en konkret sygdom dokumenteres, hvis dette ikke er angivet andre steder. "
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Hjælpemidler</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres de hjælpemidler borger selv har anskaffet.  Bevilligede hjælpemidler kan også dokumenteres eller vises her."
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
+        <div>
+          <a>Boligens indretning</a>
+          <b-button
+            pill
+            variant="outline-info"
+            data-toggle="tooltip"
+            title="Her dokumenteres både det der hæmmer og fremmer borgerens funktionsevne i hverdagen.  Kan suppleres med praktiske oplysninger fx om der er elevator, dørtrin eller trapper. "
+            >?</b-button
+          >
+          <textarea class="form-control" rows="2"></textarea>
+        </div>
       </div>
     </div>
   </div>
