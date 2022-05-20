70f809b713dc279743857784817d20eeebe8d0fb
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2022-05-20 14:33:47 +0200

    Update CitizenRepository.cs

--- SOSU2022_BackEnd/SOSU2022_BackEnd.DataAccess/Repositories/CitizenRepository.cs
+++ SOSU2022_BackEnd/SOSU2022_BackEnd.DataAccess/Repositories/CitizenRepository.cs
@@ -14,6 +14,7 @@ namespace SOSU2022_BackEnd.DataAcces.Repositories
     {
         private readonly IMongoCollection<CitizenDocument> _citizens;
         private readonly CitizenConverter _citizenConverter;
+        private readonly IMongoCollection<GeneralInformationDocument> _generals;
 
         public CitizenRepository(ICitizenDatabaseSettings settings)
         {
@@ -22,13 +23,93 @@ namespace SOSU2022_BackEnd.DataAcces.Repositories
             var db = client.GetDatabase("SOSU2022");
 
             _citizens = db.GetCollection<CitizenDocument>("Borger");
+            _generals = db.GetCollection<GeneralInformationDocument>("Generelle_oplysninger");
         }
 
         public Citizen Create(Citizen citizen)
         {
+            var gDocs = new List<GeneralInformationDocument>();
             var documentToInsert = _citizenConverter.Convert(citizen);
             _citizens.InsertOne(documentToInsert);
             var citizenToReturn = _citizenConverter.Convert(documentToInsert);
+            var generalDocToInsert1 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Mestring",
+                Tekst = ""
+            };
+            var generalDocToInsert2 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Motivation",
+                Tekst = ""
+            };
+            var generalDocToInsert11 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Ressourcer",
+                Tekst = ""
+            };
+            var generalDocToInsert3 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Roller",
+                Tekst = ""
+            };
+            var generalDocToInsert4 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Vaner",
+                Tekst = ""
+            };
+            var generalDocToInsert5 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Udannelse_job",
+                Tekst = ""
+            };
+            var generalDocToInsert6 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Livshistorie",
+                Tekst = ""
+            };
+            var generalDocToInsert7 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Helbreds_oplysninger",
+                Tekst = ""
+            };
+            var generalDocToInsert8 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Hjaelpemidler",
+                Tekst = ""
+            };
+            var generalDocToInsert9 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Boligens_indretning",
+                Tekst = ""
+            };
+            var generalDocToInsert10 = new GeneralInformationDocument
+            {
+                BorgerId = citizenToReturn.Id,
+                Emne = "Netværk",
+                Tekst = ""
+            };
+            gDocs.Add(generalDocToInsert1);
+            gDocs.Add(generalDocToInsert2);
+            gDocs.Add(generalDocToInsert3);
+            gDocs.Add(generalDocToInsert4);
+            gDocs.Add(generalDocToInsert5);
+            gDocs.Add(generalDocToInsert6);
+            gDocs.Add(generalDocToInsert7);
+            gDocs.Add(generalDocToInsert8);
+            gDocs.Add(generalDocToInsert9);
+            gDocs.Add(generalDocToInsert10);
+            gDocs.Add(generalDocToInsert11);
+            _generals.InsertMany(gDocs);
             return citizenToReturn;
         }
         
@@ -52,7 +133,12 @@ namespace SOSU2022_BackEnd.DataAcces.Repositories
 
         public void Delete(string citizenToDelete)
         {
+            var deletefilterdocs = Builders<GeneralInformationDocument>
+                .Filter.Eq("BorgerId", value:citizenToDelete);
+            
             var deleteFilter = Builders<CitizenDocument>.Filter.Eq("_id", new ObjectId(citizenToDelete));
+            
+            _generals.DeleteManyAsync(deletefilterdocs);
             _citizens.DeleteOne(deleteFilter);
         }
     }
