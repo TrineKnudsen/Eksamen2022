986a16781507daad0cf448a3628617065f2e53bd
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2022-05-23 19:13:48 +0200

    xunit

---
+++ SOSU2022_BackEnd/Core.Test/SOSU2022_BackEnd.Core.Test/CitizenTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SOSU2022_BackEnd.Core.Models;
+using Xunit;
+
+namespace SOSU2022_BackEnd.Core.Test
+{
+    public class CitizenTest
+    {
+        private readonly Citizen _citizen;
+
+        public CitizenTest()
+        {
+            _citizen = new Citizen();
+        }
+
+        [Fact]
+        public void Citizen_CanBeInitialized()
+        {
+            Assert.NotNull(_citizen);
+        }
+    }
+}
