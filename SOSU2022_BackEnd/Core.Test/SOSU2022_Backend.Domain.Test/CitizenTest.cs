2a9b52cbae5a3b7ca7c3397532c58f66d5dfd8a0
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2022-05-23 19:10:20 +0200

    ff

--- SOSU2022_BackEnd/Core.Test/SOSU2022_Backend.Domain.Test/CitizenTest.cs
+++ SOSU2022_BackEnd/Core.Test/SOSU2022_Backend.Domain.Test/CitizenTest.cs
@@ -6,17 +6,6 @@ namespace SOSU2022_Backend.Domain.Test
 {
     public class CitizenTest
     {
-        private readonly Citizen _citizen;
-
-        public CitizenTest()
-        {
-            _citizen = new Citizen();
-        }
-
-        [Fact]
-        public void Citizen_CanBeInitialized()
-        {
-            Assert.NotNull(_citizen);
-        }
+        
     }
 }
